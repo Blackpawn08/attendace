e9373d5278e14d074fc50b979d0b581ab0efd51b
IT-PSPC <corpuzkyle08@gmail.com>
2024-08-20 16:06:01 +0800

    Qr Code Storing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import profile from "../assets/profile.png"; // Ensure proper path
 import qr from "../assets/qr.png"; // Ensure proper path
 import link from "../assets/link.png";
+import flag from "../assets/flag.png";
+import raceback from "../assets/racebackg.png";
+import track from "../assets/racetrack.png";
 import foot from "../assets/footer.png";
 import { FcApproval } from "react-icons/fc";
+import tiremain from "../assets/tiremain.png";
+import QRCode from "react-qr-code";
 
 export default function Home() {
   // Initialize state for each row
@@ -22,118 +27,145 @@ export default function Home() {
       ...prevState,
       [rowKey]: true, // Mark the clicked row as approved
     }));
-    window.open(e.currentTarget.href, "_blank"); // Open link in new tab
+    window.open(e.currentTarget.href, "_blank");
+    // Open link in new tab
   };
 
+  const [qrCodeValue, setQrCodeValue] = useState("");
+  const [storedItems, setStoredItems] = useState([]);
+
+  /*   const storing = () => {
+    
+  } */
+
+  useEffect(() => {
+    const items = JSON.parse(localStorage.getItem("items")) || [];
+    setStoredItems(items);
+    console.log(`Stored: ${storedItems}`);
+  }, []);
+
   return (
-    <main className="relative min-h-screen flex flex-col justify-between">
+    <main
+      className="relative min-h-screen flex flex-col justify-between"
+      style={{
+        backgroundImage: `url(${track.src})`, // Set background image
+        backgroundSize: "cover", // Make the image cover the div
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
       <div
         className="h-48 w-full rounded-b-3xl"
         style={{
-          backgroundColor: "white",
-          backgroundImage: `url(${link.src})`, // Set background image
+          // Set background image
           backgroundSize: "cover", // Make the image cover the div
           backgroundPosition: "center",
-          opacity: "revert",
+          opacity: "95%",
         }}
       >
-        <div className="md:flex-col flex flex-row items-center justify-center gap-4 md:pt-11 pt-28">
+        <div className="md:flex-col flex flex-row items-center justify-center gap-4 md:pt-11 pt-24">
           <Image
-            className="md:h-48 md:w-48 h-32 w-32"
+            className="md:h-48 md:w-48 h-32 w-32 shadow-xl sha"
             src={profile}
             alt="profile"
           />
+
           <h1 className="md:text-4xl text-2xl font-bold text-blue-900 md:text-black bg-slate-50 md:bg-transparent  bg-opacity-50 backdrop-blur-sm rounded-xl">
-            Kyle Corpuz
+            {storedItems[storedItems.length - 1]}
           </h1>
         </div>
       </div>
       <div className="md:flex md:mt-36 mt-16 justify-between items-center md:w-full relative ">
         <div className="md:w-2/5 flex justify-center p-2 relative z-10">
-          <Image
-            className="md:h-72 md:w-72 h-40 w-40 object-contain"
-            src={qr}
+          {/* <Image
+            className="md:h-72 md:w-72 h-40 w-40 object-contain shadow-xl"
+            src={qrCodeValue}
             alt="qr-code"
+          /> */}
+
+          <QRCode
+            value={qrCodeValue}
+            className="md:h-72 md:w-72 h-40 w-40 object-contain shadow-xl"
           />
         </div>
         <div className="w-full p-3 relative z-10">
-          <table
-            className="min-w-full border-collapse border border-gray-200 rounded-lg overflow-hidden"
-            style={{ backgroundColor: "#134B70" }}
-          >
-            <thead className="rounded-2xl overflow-hidden">
+          <table className="min-w-full border-collapse border border-gray-200 rounded-lg overflow-hidden">
+            <thead
+              className="rounded-2xl overflow-hidden"
+              style={{ backgroundColor: "#134B70" }}
+            >
               <tr className="text-center">
-                <th className="px-4 py-2 text-white font-semibold md:w-9/12">
-                  Microsites
-                </th>
-                <th className="px-4 py-2 text-white font-semibold md:w-3/12">
+                <th className="px-4 py-2 p-2 text-xl text-white font-semibold md:w-3/12">
                   Status
+                </th>{" "}
+                <th className="px-4 py-2 p-2 text-xl  text-white font-semibold md:w-9/12">
+                  Stations
                 </th>
               </tr>
             </thead>
             <tbody className="text-center" style={{ color: "#134B70" }}>
-              <tr style={{ backgroundColor: "#EEEEEE", color: "#134B70" }}>
-                <td className="px-4 py-2 w-9/12">
-                  <a
-                    href="https://esl-games-demo.retsol.ph/basketball/user-form"
-                    className="text-black hover:text-blue-800"
-                    onClick={(e) => handleClick(e, "row1")} // Specific row key
-                  >
-                    Rexona Basketball
-                  </a>
-                </td>
-                <td className="px-4 py-2 w-full flex justify-center items-center">
-                  {approvedRows.row1 && <FcApproval className="w-8 h-8" />}
-                </td>
-              </tr>
+              {[
+                {
+                  key: "row1",
+                  name: "Floor Layout",
+                },
+                {
+                  key: "row2",
+                  name: "KRVR + POG",
+                },
+                {
+                  key: "row3",
+                  name: "Eveer Deen",
+                },
+                {
+                  key: "row4",
+                  name: "Woody + Buzz",
+                },
+                {
+                  key: "row5",
+                  name: "3MVAS",
+                },
+                {
+                  key: "row6",
+                  name: "StoreX",
+                },
+              ].map((row, index, array) => (
+                <tr
+                  key={row.key}
+                  style={{ backgroundColor: "#EEEEEE", color: "#134B70" }}
+                >
+                  <td className="px-4 py-2 w-full flex justify-center items-center relative">
+                    {/* Circle */}
+                    <div
+                      className={`w-8 h-8 rounded-full flex items-center justify-center ${
+                        approvedRows[row.key] ? "bg-green-500" : "bg-gray-300"
+                      }`}
+                    >
+                      {approvedRows[row.key] && (
+                        <FcApproval className="w-6 h-6" />
+                      )}
+                    </div>
 
-              <tr style={{ backgroundColor: "#EEEEEE", color: "#134B70" }}>
-                <td className="px-4 py-2 w-9/12">
-                  <a
-                    href="https://microsite-git-init-demo-renzoralphpua.vercel.app/microsite/videoplayer/sunsilk"
-                    className="text-black hover:text-blue-800"
-                    onClick={(e) => handleClick(e, "row2")} // Specific row key
-                  >
-                    With Sunsilk Commercial
-                  </a>
-                </td>
-                <td className="px-4 py-2 w-full flex justify-center items-center">
-                  {approvedRows.row2 && <FcApproval className="w-8 h-8" />}
-                </td>
-              </tr>
-
-              <tr style={{ backgroundColor: "#EEEEEE", color: "#134B70" }}>
-                <td className="px-4 py-2 w-9/12">
-                  <a
-                    href="https://microsite-git-init-demo-renzoralphpua.vercel.app/microsite/user-form/creamsilk"
-                    className="text-black hover:text-blue-800"
-                    onClick={(e) => handleClick(e, "row3")} // Specific row key
-                  >
-                    Without Sunsilk Commercial
-                  </a>
-                </td>
-                <td className="px-4 py-2 w-full flex justify-center items-center">
-                  {approvedRows.row3 && <FcApproval className="w-8 h-8" />}
-                </td>
-              </tr>
-
-              <tr style={{ backgroundColor: "#EEEEEE", color: "#134B70" }}>
-                <td className="px-4 py-2 w-9/12">
-                  <a
-                    href="https://feedback-loop-web.vercel.app/login"
-                    className="text-black hover:text-blue-800"
-                    onClick={(e) => handleClick(e, "row4")} // Specific row key
-                  >
-                    NCCC
-                  </a>
-                </td>
-                <td className="px-4 py-2 w-full flex justify-center items-center">
-                  {approvedRows.row4 && <FcApproval className="w-8 h-8" />}
-                </td>
-              </tr>
+                    {/* Connecting Line */}
+                    {index < array.length - 1 && (
+                      <div className="absolute left-1/2 transform -translate-x-1/2 top-8 w-px h-10 bg-gray-300"></div>
+                    )}
+                  </td>
+                  <td className="px-4 py-2 w-9/12">
+                    <a
+                      //href={row.href}
+                      className="text-black hover:text-blue-800 cursor-pointer"
+                      // onClick={(e) => handleClick(e, row.key)}
+                    >
+                      {row.name}
+                    </a>
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
+        {/*  <Image src={track} alt="racetrack"></Image> */}
       </div>{" "}
       <div className="relative z-0">
         <Image
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,10 +1,16 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+//import { useRouter } from "next/router";
 import Image from "next/image";
 import profile from "../../assets/profile.png"; // Ensure proper path
 import qr from "../../assets/qr.png"; // Ensure proper path
 import link from "../../assets/link.png";
 import foot from "../../assets/footer.png";
+import flag from "../../assets/flag.png";
+import raceback from "../../assets/racebackg.png";
+import track from "../../assets/racetrack.png";
+import tirebackg from "../../assets/tirebackg.png";
+import QRCode from "react-qr-code";
 
 export default function Home() {
   // Initialize state for each row
@@ -15,26 +21,55 @@ export default function Home() {
     row4: false,
   });
 
+  const [qrCodeValue, setQrCodeValue] = useState("");
+  const [inputValue, setInputValue] = useState("");
+  //const router = useRouter();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const storedItems = JSON.parse(localStorage.getItem("items")) || [];
+
+    const updatedItems = [...storedItems, inputValue];
+    console.log(`Test: ${updatedItems}`);
+    localStorage.setItem("items", JSON.stringify(updatedItems));
+    setInputValue("");
+    // router.push("/");
+  };
   return (
-    <main className="relative min-h-screen flex flex-col justify-between">
+    <main
+      className="relative min-h-screen flex flex-col "
+      style={{
+        backgroundImage: `url(${tirebackg.src})`, // Set background image
+        backgroundSize: "cover", // Make the image cover the div
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        /* backgroundColor: "#e8e4c9", */
+        backgroundColor: "#fffef5",
+      }}
+    >
       <div
-        className="h-48 w-full rounded-b-3xl flex justify-center pt-6"
+        className="h-96 w-full rounded-b-3xl relative  "
         style={{
-          backgroundColor: "white",
-          backgroundImage: `url(${link.src})`, // Set background image
-          backgroundSize: "cover", // Make the image cover the div
-          backgroundPosition: "center",
-          opacity: "revert",
+          backgroundImage: `url(${flag.src})`, // Set background image
+          backgroundSize: "contain", // Make the image cover the div
+          backgroundPosition: "right",
+          backgroundRepeat: "no-repeat",
         }}
-      ></div>
-      <div className="w-full flex justify-center items-center p-5 pt-20 md:pt-4">
+      >
+        {" "}
+        {/*  {qrCodeValue != "" && <QRCode value={qrCodeValue} />} */}
+        {qrCodeValue != "" && <QRCode value={qrCodeValue} />}
+      </div>
+
+      <div className="w-full flex justify-center items-center absolute md:top-60 top-72 ">
         <form
-          /*onSubmit={handleSubmit}*/ className="p-7 text-center rounded-xl shadow-lg"
+          /*onSubmit={handleSubmit}*/ className="p-7 text-center rounded-xl shadow-4xl w-96 "
           style={{ backgroundColor: "#134B70" }}
+          onSubmit={handleSubmit}
         >
           <label
             htmlFor="simpleInput"
-            className="block mb-2 font-medium text-white text-2xl"
+            className="block mb-2 font-medium text-white text-3xl"
           >
             Enter your Name
           </label>
@@ -42,26 +77,31 @@ export default function Home() {
             type="text"
             id="simpleInput"
             className="border border-gray-300 p-2 rounded mb-4 w-full shadow-lg "
-            //value={inputValue}
+            value={inputValue}
+            /*  onChange={(e) => {
+              setQrCodeValue(e.target.value);
+            }}
+            placeholder="Type here..." */
             onChange={(e) => setInputValue(e.target.value)}
             placeholder="Type here..."
           />
+
           <button
             type="submit"
-            className=" text-black p-2 rounded shadow-lg "
+            className=" text-black font-semibold p-2 rounded shadow-lg "
             style={{ backgroundColor: "#E2E2B6" }}
           >
             Submit
           </button>
         </form>
       </div>
-      <div className="relative">
+      {/* <div className="relative">
         <Image
           src={foot}
           alt="foot"
           className="md:h-20 object-cover overflow-visible"
         />
-      </div>
+      </div> */}
     </main>
   );
 }
