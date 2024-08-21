906215363aebb041e0d2da090e1093e78a1f3a70
IT-PSPC <corpuzkyle08@gmail.com>
2024-08-21 10:15:27 +0800

    UI fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-"use client";  // Ensure this component is treated as client-side
+"use client"; // Ensure this component is treated as client-side
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Image from "next/image";
@@ -19,36 +19,36 @@ export default function Home() {
   const [qrCodeValue, setQrCodeValue] = useState(null);
   useEffect(() => {
     const fetchUserAndData = async () => {
-      const storedUser = localStorage.getItem('user');
+      const storedUser = localStorage.getItem("user");
       if (storedUser) {
         const user = JSON.parse(storedUser);
         setUser(user);
         setQrCodeValue(user._id);
         try {
-          const response = await axios.get('http://192.168.28.63:5000/api/fetch/user', {
-            params: {
-              id: user._id,
-            },
-          });
-  
+          const response = await axios.get(
+            "http://192.168.28.63:5000/api/fetch/user",
+            {
+              params: {
+                id: user._id,
+              },
+            }
+          );
+
           if (response.status === 200) {
             setData(response.data.data);
-         
           } else {
-            console.error('Failed to fetch data');
+            console.error("Failed to fetch data");
           }
         } catch (error) {
-          console.error('Error fetching data:', error.message);
+          console.error("Error fetching data:", error.message);
         }
       } else {
-        console.log('No user data found in localStorage');
+        console.log("No user data found in localStorage");
       }
     };
-  
+
     fetchUserAndData();
   }, []); // Dependency on `user` to refetch when it changes
-  
-
 
   return (
     <main
@@ -69,16 +69,22 @@ export default function Home() {
           opacity: "95%",
         }}
       >
-        <div className="md:flex-col flex flex-row items-center justify-center gap-4 md:pt-11 pt-24">
+        <div
+          className="md:flex-col flex flex-col-reverse items-center justify-center gap-4 md:pt-11 pt-14 bg-blue-600 pb-14 "
+          style={{
+            borderBottomLeftRadius: "50%",
+          }}
+        >
           <Image
-            className="md:h-48 md:w-48 h-32 w-32 shadow-xl sha"
+            className="md:h-48 md:w-48 h-32 w-32 shadow-xl "
             src={profile}
             alt="profile"
           />
 
-          <h1 className="md:text-4xl text-2xl font-bold text-blue-900 md:text-black bg-slate-50 md:bg-transparent  bg-opacity-50 backdrop-blur-sm rounded-xl">
-          {data?.name}
-            </h1>
+          <h1 className="md:text-4xl text-2xl font-bold text-white md:text-black  md:bg-transparent  ">
+            {data?.name}
+            Kyle Corpuz
+          </h1>
         </div>
       </div>
       <div className="md:flex md:mt-36 mt-16 justify-between items-center md:w-full relative ">
@@ -89,14 +95,12 @@ export default function Home() {
             alt="qr-code"
           /> */}
 
-{qrCodeValue && (
-  <QRCode
-    value={qrCodeValue}
-    className="md:h-72 md:w-72 h-40 w-40 object-contain shadow-xl"
-  />
-)}
-
-     
+          {qrCodeValue && (
+            <QRCode
+              value={qrCodeValue}
+              className="md:h-72 md:w-72 h-40 w-40 object-contain shadow-xl"
+            />
+          )}
         </div>
         <div className="w-full p-3 relative z-10">
           <table className="min-w-full border-collapse border border-gray-200 rounded-lg overflow-hidden">
@@ -119,13 +123,12 @@ export default function Home() {
                   <a
                     href="https://esl-games-demo.retsol.ph/basketball/user-form"
                     className="text-black hover:text-blue-800"
-                  
                   >
                     Rexona Basketball
                   </a>
                 </td>
                 <td className="px-4 py-2 w-full flex justify-center items-center">
-                {data?.station1 ? "yes" : "no"}
+                  {data?.station1 ? "yes" : "no"}
                 </td>
               </tr>
 
@@ -134,13 +137,12 @@ export default function Home() {
                   <a
                     href="https://microsite-git-init-demo-renzoralphpua.vercel.app/microsite/videoplayer/sunsilk"
                     className="text-black hover:text-blue-800"
-                    
                   >
                     With Sunsilk Commercial
                   </a>
                 </td>
                 <td className="px-4 py-2 w-full flex justify-center items-center">
-                {data?.station2 ? "yes" : "no"}
+                  {data?.station2 ? "yes" : "no"}
                 </td>
               </tr>
 
@@ -149,13 +151,12 @@ export default function Home() {
                   <a
                     href="https://microsite-git-init-demo-renzoralphpua.vercel.app/microsite/user-form/creamsilk"
                     className="text-black hover:text-blue-800"
-                    
                   >
                     Without Sunsilk Commercial
                   </a>
                 </td>
                 <td className="px-4 py-2 w-full flex justify-center items-center">
-                {data?.station3 ? "yes" : "no"}
+                  {data?.station3 ? "yes" : "no"}
                 </td>
               </tr>
 
@@ -164,13 +165,16 @@ export default function Home() {
                   <a
                     href="https://feedback-loop-web.vercel.app/login"
                     className="text-black hover:text-blue-800"
-                    
                   >
                     NCCC
                   </a>
                 </td>
                 <td className="px-4 py-2 w-full flex justify-center items-center">
-                {data?.station4 ? "yes" : "no"}
+                  {data?.station4 ? (
+                    <h1 className="h-5 w-5 bg-green-400 rounded-full" />
+                  ) : (
+                    <h1 className="h-5 w-5 bg-red-700 rounded-full" />
+                  )}
                 </td>
               </tr>
             </tbody>
